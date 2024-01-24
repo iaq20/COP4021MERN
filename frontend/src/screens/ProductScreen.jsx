ce9cd82de304c652a7295e633a9cf1edbf52ff56
iaq20 <ivan13q13@gmail.com>
2024-01-24 01:09:06 -0500

    Fetching Products from Backend instead of Frontend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,13 +1,23 @@
-import { useParams } from "react-router-dom"
-import products from "../products"
+import { useEffect, useState } from 'react';
+import { useParams} from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from "react-bootstrap";
 import Ratings from "../components/Ratings";
+import axios from "axios";
 
 const ProductScreen = () => {
+    const [product, setProduct] = useState({}); 
 
     const { id: productId } = useParams(); 
-    const product = products.find((p) => p._id === productId);
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const {data } = await axios.get(`/api/products/${productId}`); 
+            setProduct(data);
+        }
+
+        fetchProduct();
+    }, [productId]);
 
   return (
   <>
