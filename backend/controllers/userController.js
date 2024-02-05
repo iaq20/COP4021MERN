2acc6c5cf1d417c40519f8539202b30ae42f8383
iaq20 <ivan13q13@gmail.com>
2024-02-05 11:55:01 -0500

    Including Features for backend for Logout user, updateUserProfile and generating tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middleware/asyncHandler.js";
-import jwt from 'jsonwebtoken';
+import generateToken from "../utils/generateToken.js";
 
 // @desc auth user
 //@route POST/api/users/login
@@ -12,19 +12,9 @@ const authuser = asyncHandler(async (req, res) => {
     const user = await User.findOne({ email});
 
     if (user && (await user.matchPassword(password))) {
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-            expiresIn: '30d'
-        });
-        
-        // set jwt as http only cookie 
-        res.cookie('jwt', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
-            sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
-        });
+        generateToken(res,user._id);
 
-        res.json({
+        res.status(200).json({
             _id: user._id,
             name: user.name,
             email: user.email,
@@ -41,28 +31,97 @@ const authuser = asyncHandler(async (req, res) => {
 //@route POST/api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.send('register user');
+    //res.send('register user');
+    const { name, email, password} = req.body;
+
+    const userExists = await User.findOne({email}); 
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name, 
+        email,
+        password
+    });
+
+    if (user) {
+        generateToken(res,user._id);
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+    res.status(401);
+    throw new Error('Invalid user data');
+    }
 });
 
 // @desc logout user && clear cookie
 //@route POST/api/users/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.send('logout user');
+    //res.send('logout user');
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0),
+    });
+
+    res.status(200).json({ message: 'Logged out successfully'});
 });
 
 // @desc get user profile
 //@route GET/api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('get user profile');
+    //res.send('get user profile');
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(401);
+        throw new Error('User not found');
+    }
 });
 
 // @desc auth user
 //@route PUT/api/users/profile
 // @access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.send('update user profile');
+    //res.send('update user profile');
+
+    const user = await User.findById(req.user._id);
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if(req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updateUser = await user.save();
+
+        res.status(201).json({
+            _id: updateUser._id,
+            name: updateUser.name,
+            email: updateUser.email,
+            isAdmin: updateUser.isAdmin,
+        });
+    } else {
+        res.status(401);
+        throw new Error('User not found');
+    }
 });
 
 // @desc  Get user
@@ -90,7 +149,9 @@ const getUserById = asyncHandler(async (req, res) => {
 //@route PUT/api/users
 // @access Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-    res.send('update user');
+    //res.send('update user');
+
+    
 });
 
 export {
