b77564954ae0d49d53f6ca8193b958645ee31804
iaq20 <ivan13q13@gmail.com>
2024-02-05 10:52:12 -0500

    Adding userRoutes and userController

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv'; 
 dotenv.config(); 
 import connectDB from './config/db.js';
-import router from './routes/productRoutes.js'; 
+import productRoutes from './routes/productRoutes.js'; 
+import userRoutes from './routes/userRoutes.js'; 
 import { notFound, errorHandler} from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5001;
 
@@ -10,12 +11,17 @@ connectDB();
 
 const app = express();
 
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.send('API is running...');
 
 });
 
-app.use('/api/products', router); 
+app.use('/api/products', productRoutes); 
+app.use('/api/users', userRoutes); 
 
 app.use(notFound);
 app.use(errorHandler); 
